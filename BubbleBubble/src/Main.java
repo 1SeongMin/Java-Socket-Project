02663e529f0d9f0ae1323a5fc1b308b0a9dbac0f
DESKTOP-9A0RCII\Kimjaewook <jaewook3026@naver.com>
2023-11-16 21:25:51 +0900

    test

--- BubbleBubble/src/Main.java
+++ BubbleBubble/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello my name is seongmin");
-
+		System.out.println("hello my name is jaewook");
+		System.out.println("hello");
 	}
-
 }
