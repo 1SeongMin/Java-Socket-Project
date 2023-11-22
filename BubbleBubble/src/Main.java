a560ffd986317e2bf6b7094b522665a2732b4386
Seongmin <arcn1345@gmail.com>
2023-11-22 19:09:23 +0900

    main

--- BubbleBubble/src/Main.java
+++ BubbleBubble/src/Main.java
@@ -1,12 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Main {
+public class Main extends JFrame {
 
 	public static void main(String[] args) {
-		System.out.println("hello my name is seongmin");
-		System.out.println("hello my name is jaewook");
-		System.out.println("hello");
-		System.out.println("my name is jaewook");
-		System.out.println("테스트");
-		System.out.println("테스트2");
+		JFrame frame = new JFrame("시작 화면");
+	        
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        //버튼
+        JButton startBtn = new JButton("게임 시작");
+        JButton rankingBtn = new JButton("순위 표");
+        startBtn.setBounds(300,170,100,30);
+        rankingBtn.setBounds(300, 230,100,30);
+        
+        frame.getContentPane().add(startBtn);
+        frame.getContentPane().add(rankingBtn);
+        
+        //시작 화면 이미지 추가해야 함!!
+
+        frame.setVisible(true);
 	}
 }
