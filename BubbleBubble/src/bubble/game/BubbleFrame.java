c4d9aaadeff768e1dc35d006c6bd09fa00d5f9de
Seongmin <arcn1345@gmail.com>
2023-11-30 03:26:24 +0900

    레이아웃, 위치, 움직임 수정

---
+++ BubbleBubble/src/bubble/game/BubbleFrame.java
@@ -0,0 +1,170 @@
+package bubble.game;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import bubble.game.component.Enemy;
+import bubble.game.component.GameOver;
+import bubble.game.component.Player;
+import bubble.game.music.BGM;
+import bubble.game.state.EnemyWay;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BubbleFrame extends JFrame {
+ 
+	private BubbleFrame mContext = this;
+	private JLabel backgroundMap;
+	private Player player;
+	private List<Enemy> enemys; // 컬렉션으로 관리
+	private BGM bgm;
+	private GameOver gameOver;
+	private static int i;
+	
+	private int life1;
+
+	public BubbleFrame(int i) {
+		this.i = i;
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		if(i == 1) backgroundMap = new JLabel(new ImageIcon("image/round01.png"));
+		else if (i == 2 ) backgroundMap = new JLabel(new ImageIcon("image/round02.png"));
+		else backgroundMap = new JLabel(new ImageIcon("image/round03.png"));
+		
+		setContentPane(backgroundMap);
+		player = new Player(mContext);
+		add(player);
+		enemys = new ArrayList<Enemy>();
+		enemys.add(new Enemy(mContext, EnemyWay.RIGHT));
+		enemys.add(new Enemy(mContext, EnemyWay.LEFT));
+		for(Enemy e : enemys) add(e);
+		bgm = new BGM();
+		bgm.playBGM("bgm.wav");
+	}
+
+	private void initSetting() {
+		setSize(1000, 780); //1000, 640
+		setBackground(new Color(0, 0, 0));
+		setLayout(null); // absoulte 레이아웃 (자유롭게 그림을 그릴 수 있다)
+		setLocationRelativeTo(null); // JFrame 가운데 배치하기
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 창을 끌 때 JVM 같이 종료하기
+		
+		//윗 줄
+				JLabel player1 = new JLabel("name1");
+				player1.setForeground(new Color(0, 255, 0));
+				player1.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				player1.setBounds(10, 5, 50, 20);
+				add(player1);
+				
+				JLabel player2 = new JLabel("name2");
+				player2.setForeground(new Color(255, 0, 255));
+				player2.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				player2.setBounds(925, 5, 50, 20);
+				add(player2);
+				
+				JLabel score = new JLabel("Score");
+				score.setForeground(new Color(255, 0, 0));
+				score.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				score.setBounds(470, 5, 50, 20);
+				add(score);
+				
+				//아랫줄 
+				JLabel player1_score = new JLabel("score1");
+				player1_score.setForeground(new Color(255, 255, 255));
+				player1_score.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				player1_score.setBounds(10, 30, 70, 20);
+				add(player1_score);
+				
+				JLabel player2_score = new JLabel("score2");
+				player2_score.setForeground(new Color(255, 255, 255));
+				player2_score.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				player2_score.setBounds(925, 30, 70, 20);
+				add(player2_score);
+				
+				JLabel total_score = new JLabel("T score");
+				total_score.setForeground(new Color(255, 255, 255));
+				total_score.setFont(new Font("D2Coding", Font.PLAIN, 20));
+				total_score.setBounds(465, 30, 70, 20);
+				add(total_score);
+				
+				JLabel player1_life1 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player1_life1.setBounds(20, 685, player1_life1.getIcon().getIconWidth(), player1_life1.getIcon().getIconHeight());
+				add(player1_life1);
+				JLabel player1_life2 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player1_life2.setBounds(55, 685, player1_life2.getIcon().getIconWidth(), player1_life2.getIcon().getIconHeight());
+				add(player1_life2);
+				JLabel player1_life3 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player1_life3.setBounds(90, 685, player1_life3.getIcon().getIconWidth(), player1_life3.getIcon().getIconHeight());
+				add(player1_life3);
+				
+				JLabel player2_life1 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player2_life1.setBounds(925, 685, player2_life1.getIcon().getIconWidth(), player2_life1.getIcon().getIconHeight());
+				add(player2_life1);
+				JLabel player2_life2 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player2_life2.setBounds(890, 685, player2_life2.getIcon().getIconWidth(), player2_life2.getIcon().getIconHeight());
+				add(player2_life2);
+				JLabel player2_life3 = new JLabel(new ImageIcon("image/life_heart.png"));
+				player2_life3.setBounds(855, 685, player2_life3.getIcon().getIconWidth(), player2_life3.getIcon().getIconHeight());
+				add(player2_life3);
+	}
+
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+
+			// 키보드 클릭 이벤트 핸들러
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT: 
+					if (!player.isLeft() && !player.isLeftWallCrash() && player.getState() == 0)
+						player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					if (!player.isRight() && !player.isRightWallCrash() && player.getState() == 0)
+						player.right();
+					break;
+				case KeyEvent.VK_UP:
+					if(!player.isUp() && !player.isDown() && player.getState() == 0)
+						player.up();
+					break;
+				case KeyEvent.VK_SPACE:
+					if(player.getState() == 0)
+						player.attack();
+					break;
+				}
+			}
+
+			// 키보드 해제 이벤트 핸들러
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+
+		});
+	}
+
+	public static void main(String[] args) {
+		new BubbleFrame(i);
+	}
+}
---
+++ BubbleBubble/src/bubble/game/component/Level.java
@@ -0,0 +1,114 @@
+package bubble.game.component;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import bubble.game.BubbleFrame;
+
+public class Level extends JFrame {
+	
+	private ImageIcon img_icon = new ImageIcon("image/game_intro.png");
+	private Font font1 = new Font("D2Coding", Font.CENTER_BASELINE, 20);
+	private JFrame frame;
+	private JButton startBtn;
+	private JLabel img_label;
+	
+	public Level() {
+		setFrame();
+		setStartButton1();
+		setStartButton2();
+		setStartButton3();
+		setImage();
+	}
+	
+	public void setStartButton1() {
+		startBtn = new JButton("LEVEL 1");
+        startBtn.setBounds(176,482,200,50);
+        startBtn.setBorderPainted(false); // 버튼 테두리 설정
+        startBtn.setContentAreaFilled(false); // 버튼 영역 배경 표시 설정
+        startBtn.setFocusPainted(false); // 포커스 표시 설정
+        startBtn.setFont(font1); //폰트 설정
+        startBtn.setForeground(new Color(0, 0, 255)); //글자 색 설정
+        frame.getContentPane().add(startBtn); //시작 버튼 추가
+        
+        //시작 버튼에 대한 ActionListener 
+        addActionToStartButton1();
+    }
+	public void addActionToStartButton1() {
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // 시작 화면 프레임 닫기
+                new BubbleFrame(1); // BubbleGame 호출
+            }
+        });
+    }
+	
+	public void setStartButton2() {
+		startBtn = new JButton("LEVEL 2");
+        startBtn.setBounds(410,482,200,50);
+        startBtn.setBorderPainted(false); // 버튼 테두리 설정
+        startBtn.setContentAreaFilled(false); // 버튼 영역 배경 표시 설정
+        startBtn.setFocusPainted(false); // 포커스 표시 설정
+        startBtn.setFont(font1); //폰트 설정
+        startBtn.setForeground(new Color(0, 0, 255)); //글자 색 설정
+        frame.getContentPane().add(startBtn); //시작 버튼 추가
+        
+        //시작 버튼에 대한 ActionListener 
+        addActionToStartButton2();
+    }
+	public void addActionToStartButton2() {
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // 시작 화면 프레임 닫기
+                new BubbleFrame(2); // BubbleGame 호출
+            }
+        });
+    }
+	
+	public void setStartButton3() {
+		startBtn = new JButton("LEVEL 3");
+        startBtn.setBounds(651,482,200,50);
+        startBtn.setBorderPainted(false); // 버튼 테두리 설정
+        startBtn.setContentAreaFilled(false); // 버튼 영역 배경 표시 설정
+        startBtn.setFocusPainted(false); // 포커스 표시 설정
+        startBtn.setFont(font1); //폰트 설정
+        startBtn.setForeground(new Color(0, 0, 255)); //글자 색 설정
+        frame.getContentPane().add(startBtn); //시작 버튼 추가
+        
+        //시작 버튼에 대한 ActionListener 
+        addActionToStartButton3();
+    }
+	public void addActionToStartButton3() {
+        startBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose(); // 시작 화면 프레임 닫기
+                new BubbleFrame(3); // BubbleGame 호출
+            }
+        });
+    }
+	
+	
+	public void setImage() {
+		img_label = new JLabel(img_icon);
+		img_label.setBounds(15, -200, img_icon.getIconWidth(), img_icon.getIconWidth());
+		frame.getContentPane().add(img_label);
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setVisible(true);
+	}
+	
+	public void setFrame() {	
+		frame = new JFrame("레벨 화면");
+        frame.setSize(1000, 700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+	}
+
+	public static void main(String[] args) {
+		new Level();
+	}
+}
